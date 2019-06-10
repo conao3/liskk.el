09e82c1bc637e46fbd373bc19ce319580392a535
conao3 <conao3@gmail.com>
2019-06-11 01:58:50 +0900

    fix comment

--- liskk.el
+++ liskk.el
@@ -324,27 +324,28 @@ Date: Wed, 10 Jun 1998 19:06:11 +0900 (JST)
       (progn
         ;; 次の状態に遷移できた
         ;;  - 状態を更新する
+        ;;  - 次状態遷移可能性を検証する
         (setq liskk-current-rule-node (assoc key (nth 4 liskk-current-rule-node)))
 
-        ;; 次の状態がない
-        ;;  - 根に戻る
-        ;;  - 最終の状態で`liskk-kana-insert'を実行する
-        ;;    - 変換前の英字を消す
-        ;;    - 現在の葉の文字列を挿入する
-        ;;    - 葉に次状態の指定があれば、それを処理器に投入する
         (unless (nth 4 liskk-current-rule-node)
+          ;; 遷移できたが、次の状態がない
+          ;;  - 根に戻る
+          ;;  - 最終の状態で`liskk-kana-insert'を実行する
+          ;;    - 変換前の英字を消す
+          ;;    - 現在の葉の文字列を挿入する
+          ;;    - 葉に次状態の指定があれば、それを処理器に投入する
           (let ((node liskk-current-rule-node))
             (setq liskk-current-rule-node nil)
             (liskk-kana-insert node))))
 
-    ;; 次の状態に遷移できない
-    ;;   - 根に戻る
-    ;;   - 最終の状態で`liskk-kana-insert'を実行する
-    ;;     - 変換前の英字を消す
-    ;;     - 現在の葉の文字列を挿入する
-    ;;     - 葉に次状態の指定があれば、それを処理器に投入する
-    ;;   - 入力されたキーを、次のローマ字列として処理器に投入する
     (let ((node liskk-current-rule-node))
+      ;; 次の状態に遷移できない
+      ;;   - 根に戻る
+      ;;   - 最終の状態で`liskk-kana-insert'を実行する
+      ;;     - 変換前の英字を消す
+      ;;     - 現在の葉の文字列を挿入する
+      ;;     - 葉に次状態の指定があれば、それを処理器に投入する
+      ;;   - 入力されたキーを、次のローマ字列として処理器に投入する
       (setq liskk-current-rule-node nil)
       (liskk-kana-insert node)
       (liskk-kana-input key)))
